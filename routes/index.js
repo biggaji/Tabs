b8212e48d9ef49171ebdc0fe1fdc84cd319cae70
biggaji <tobiajibade2017@yandex.com>
2021-05-23 09:47:04 -0700

    added fonts to app

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', (req,res) => {
 	res.render("index", {pageTitle: "Homepage"});
 });
 
+router.get('/about', (req,res) => {
+	res.render("about", {pageTitle: "About me"});
+});
+
+router.get('/contact', (req,res) => {
+	res.render("contact", {pageTitle: "Contact"});
+});
+
+router.post('/contact', (req,res) => {
+	// 
+});
 module.exports = router;
